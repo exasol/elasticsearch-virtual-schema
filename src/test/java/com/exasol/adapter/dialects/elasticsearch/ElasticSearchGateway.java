3049b6b5757f841027e621028e04f7e2a52d875a
Marcelo Chiaradia <chiaradiamarcelo@gmail.com>
2020-12-14 17:43:38 +0100

    \#3: Integration tests for Main capabilities

---
+++ src/test/java/com/exasol/adapter/dialects/elasticsearch/ElasticSearchGateway.java
@@ -0,0 +1,83 @@
+package com.exasol.adapter.dialects.elasticsearch;
+
+import java.io.IOException;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.*;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.license.StartTrialRequest;
+import org.elasticsearch.common.xcontent.XContentType;
+
+/**
+ * Gateway for interacting with ElasticSearch data source.
+ */
+public class ElasticSearchGateway {
+    private final RestHighLevelClient client;
+
+    private ElasticSearchGateway(final String httpHostAddress) {
+        this.client = new RestHighLevelClient(RestClient.builder(HttpHost.create(httpHostAddress)));
+    }
+
+    /**
+     * Connect to an ElasticSearch data source in the given httpHostAddress and the standard port.
+     *
+     * @param httpHostAddress
+     */
+    public static ElasticSearchGateway connectTo(final String httpHostAddress) {
+        return new ElasticSearchGateway(httpHostAddress);
+    }
+
+    /**
+     * Index a new document in Json format in the data source.
+     *
+     * @param indexName
+     * @param jsonSource
+     * @throws IOException
+     */
+    public void indexDocument(final String indexName, final String jsonSource) throws IOException {
+        final IndexRequest indexRequest = new IndexRequest(indexName).source(jsonSource, XContentType.JSON);
+        this.client.index(indexRequest, RequestOptions.DEFAULT);
+    }
+
+    /**
+     * Create an index in the data source with the given name.
+     *
+     * @param indexName
+     * @throws IOException
+     */
+    public void createIndex(final String indexName) throws IOException {
+        this.client.indices().create(new CreateIndexRequest(indexName), RequestOptions.DEFAULT);
+    }
+
+    /**
+     * Drop an index in the data source that matches the given name.
+     *
+     * @param indexName
+     * @throws IOException
+     */
+    public void dropIndex(final String indexName) throws IOException {
+        this.client.indices().delete(new DeleteIndexRequest(indexName), RequestOptions.DEFAULT);
+    }
+
+    /**
+     * Start the trial license, required for performing JDBC operations on the data source.
+     *
+     * @throws IOException
+     */
+    public void startTrial() throws IOException {
+        this.client.license().startTrial(new StartTrialRequest(true), RequestOptions.DEFAULT);
+    }
+
+    /**
+     * Close the connection to the data source.
+     */
+    public void closeConnection() {
+        try {
+            this.client.close();
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/com/exasol/adapter/dialects/elasticsearch/ITConfiguration.java
@@ -0,0 +1,35 @@
+package com.exasol.adapter.dialects.elasticsearch;
+
+import java.nio.file.Path;
+
+final class ITConfiguration {
+    private static final String DEFAULT_EXASOL_DOCKER_IMAGE_REFERENCE = "7.0.2";
+    static final String ELASTICSEARCH_DOCKER_IMAGE_REFERENCE = "docker.elastic.co/elasticsearch/elasticsearch:7.10.1";
+    static final String VIRTUAL_SCHEMAS_JAR_NAME_AND_VERSION = "virtual-schema-dist-8.0.0-elasticsearch-1.0.0.jar";
+    static final String TEST_RESOURCES_PATH = "src/test/resources/integration/";
+    static final String JDBC_DRIVER_NAME = "x-pack-sql-jdbc.jar";
+    static final String SETTINGS_FILE_NAME = "settings.cfg";
+    static final String DOCKER_IP_ADDRESS = "172.17.0.1";
+    static final String DEFAULT_BUCKET_PATH = "/buckets/bfsdefault/default/";
+    static final String JDBC_DRIVERS_IN_BUCKET_PATH = "drivers/jdbc/";
+    static final Path VIRTUAL_SCHEMAS_JAR_PATH = Path.of("target", VIRTUAL_SCHEMAS_JAR_NAME_AND_VERSION);
+    static final Path JDBC_DRIVER_PATH = Path.of("target/elasticsearch-driver/" + JDBC_DRIVER_NAME);
+    static final Path SETTINGS_FILE_PATH = Path.of(TEST_RESOURCES_PATH + SETTINGS_FILE_NAME);
+
+    private ITConfiguration() {
+        // prevent instantiation
+    }
+
+    /**
+     * Get the Exasol {@code docker-db} image reference.
+     * <p>
+     * This reference can be overridden by setting the Java property {@code com.exasol.dockerdb.image}. If the property
+     * is not set, then the default reference provided with the integration tests is used instead.
+     * </p>
+     *
+     * @return reference to the Exasol {@code docker-db} image.
+     */
+    public static String getExasolDockerImageReference() {
+        return System.getProperty("com.exasol.dockerdb.image", DEFAULT_EXASOL_DOCKER_IMAGE_REFERENCE);
+    }
+}
