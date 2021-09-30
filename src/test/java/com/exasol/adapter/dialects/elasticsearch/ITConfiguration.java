802fa77ec10a9e0ea52f6bf0d70644062b2ce31d
Christoph Pirkl <christoph.pirkl@exasol.com>
2021-09-30 10:35:10 +0200

    Update docker image versions

--- src/test/java/com/exasol/adapter/dialects/elasticsearch/ITConfiguration.java
+++ src/test/java/com/exasol/adapter/dialects/elasticsearch/ITConfiguration.java
@@ -3,8 +3,8 @@ package com.exasol.adapter.dialects.elasticsearch;
 import java.nio.file.Path;
 
 final class ITConfiguration {
-    private static final String DEFAULT_EXASOL_DOCKER_IMAGE_REFERENCE = "7.0.2";
-    static final String ELASTICSEARCH_DOCKER_IMAGE_REFERENCE = "docker.elastic.co/elasticsearch/elasticsearch:7.10.1";
+    private static final String DEFAULT_EXASOL_DOCKER_IMAGE_REFERENCE = "7.1.1";
+    static final String ELASTICSEARCH_DOCKER_IMAGE_REFERENCE = "docker.elastic.co/elasticsearch/elasticsearch:7.15.0";
     static final String VIRTUAL_SCHEMAS_JAR_NAME_AND_VERSION = "virtual-schema-dist-9.0.3-elasticsearch-2.0.2.jar";
     static final String TEST_RESOURCES_PATH = "src/test/resources/integration/";
     static final String JDBC_DRIVER_NAME = "x-pack-sql-jdbc.jar";
